0540dbe1c3807518e244369c535ba0c038a3f54a
JuanSolar <juansolarsolar@yahoo.com>
2023-04-27 03:32:03 +0000

    resulto hasta 8

--- .learn/resets/04-User_inputed_variables/app.js
+++ .learn/resets/04-User_inputed_variables/app.js
@@ -1,6 +1,3 @@
 let age = prompt('What is your age?');
 
 // Your code below:
-
-let result = parseInt(age) + 10;
-console.log(result);
--- exercises/04-User_inputed_variables/app.js
+++ exercises/04-User_inputed_variables/app.js
@@ -1,3 +1,6 @@
 let age = prompt('What is your age?');
 
 // Your code below:
+let resultado =parseInt(age) +10;
+console.log(resultado)
+ 
